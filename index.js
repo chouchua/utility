1f705cbb3c58407c8f73be050b53e00213d1784b
Chuan-Keng Chou <chouchua@Chuan-Kengs-MacBook-Pro.local>
2016-06-27 23:39:13 -0400

    add mkdir functionality

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ function utils(){
     /**
      * print object
      */
-    this.export = function (content,location){
+    this.export = function (location,content){
         var json = JSON.stringify(content);
-        fs.writeFileSync(json,location);
+        fs.writeFileSync(location,json);
     }
+    this.logger= function(content){
+        console.log(JSON.stringify(content));
+        fs.writeFileSync('./send/log.txt',JSON.stringify(content));
+    }
+}
+function ensureDirExists(filePath){
+    var dirname = path.dirname(filePath);
+    console.log(dirname);
+    // fs.mkdirSync(dirname);
 }
 
 module.exports =  utils;
